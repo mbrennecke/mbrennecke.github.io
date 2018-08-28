4d7b5d68b7d674458a6accefeee3b34f57f3d833
Matthew Brennecke <mbrennecke10@gmail.com>
2018-08-27 18:24:43 -0600

    liri update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -71,12 +71,11 @@ $(document).ready(function() {
 		$(".body-container").append(
 		'<div class="row">' +
 			'<div class="col-sm">' +
-			'<a href="https://jpmatlock.github.io/Group-Project-1/" target="_blank"><img src="assets/images/beer-buddies.jpg" alt="Beer Bound" class="image img-responsive" /></a>' + 
+			'<a href="https://mbrennecke.github.io/Group-Project-1/" target="_blank"><img src="assets/images/beer-buddies.jpg" alt="Beer Bound" class="image img-responsive" /></a>' + 
 			'<h2 class="portfolio-p">' +
-			'<a href="https://jpmatlock.github.io/Group-Project-1/" target="_blank">Beer Bound</a>' + 
+			'<a href="https://mbrennecke.github.io/Group-Project-1/" target="_blank">Beer Bound</a>' + 
 			'<br/>' +
-			'<a href="https://github.com/pmatlock/Group-Project-1" class="repo" target="_blank">-Github Repo-</a>' +
-			
+			'<a href="https://github.com/mbrennecke/Group-Project-1" class="repo" target="_blank">-Github Repo-</a>' +
 			'</h2></div>' +
 			'<div class="col-sm">' +
 			'<a href="https://mbrennecke.github.io/unit-4-game/" target="_blank"><img src="assets/images/rpg.jpg" alt="RPG game" class="image img-responsive" /></a>' +
@@ -85,7 +84,13 @@ $(document).ready(function() {
 			'<br/>' +
 			'<a href="https://github.com/mbrennecke/unit-4-game" class="repo" target="_blank">-Github Repo-</a>' +
 			'</h2></div>' +
-			'<div class="col-sm"></div>' +
+			'<div class="col-sm">' +
+			'<a href="https://github.com/mbrennecke/liri-node-app" target="_blank"><img src="assets/images/liri.jpg" alt="Liri cmd" class="image img-responsive" /></a>' +
+			'<h2 class="portfolio-p">' +
+			'<a href="https://github.com/mbrennecke/liri-node-app" target="_blank">Liri node app</a>' +
+			'<br/>' +
+			'<a href="https://github.com/mbrennecke/liri-node-app" class="repo" target="_blank">-Github Repo-</a>' +
+			'</h2></div>' +
 			'</div><div class="row">' +
 			'<div class="col-sm">' + 
 			'<a href="https://mbrennecke.github.io/TriviaGame/" target="_blank"><img src="assets/images/qa.jpg" alt="trivia game" class="image img-responsive" /></a>' +
