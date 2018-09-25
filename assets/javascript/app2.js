b08da2a059f794fc0d73b1d0fa417c507cd09b8a
MBrennecke <MBrennecke10@gmail.com>
2018-09-25 09:38:01 -0600

    updated links

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -8,23 +8,15 @@ var links = [
 	},{
 	link : "https://mbrennecke.github.io/TriviaGame/", image : "qa.jpg", title : "Trivia Game", repo : "https://github.com/mbrennecke/TriviaGame/"
 	},{
-	link : "https://mbrennecke.github.io/Giftastic/", image : "giftastic.jpg", title : "Giftastic", repo : "https://github.com/mbrennecke/Giftastic"
+	link : "https://dry-meadow-80045.herokuapp.com/", image : "burgertime.jpg", title : "Burgertime Sequelized", repo : "https://github.com/mbrennecke/sequelizedburger/"
 	},{
 	link : "https://mbrennecke.github.io/unit-4-game/", image : "rpg.jpg", title : "Fantasy Card Game", repo : "https://github.com/mbrennecke/unit-4-game/"
 	},{
 	link : "https://drive.google.com/file/d/1z5EtFKRMxSQ0I-1UWc9Zl2MLrpBetu1N/view?usp=sharing", image : "liri.jpg", title : "Liri node app", repo : "https://github.com/mbrennecke/liri-node-app/"
 	},{
-	link : "https://tranquil-waters-40302.herokuapp.com/", image : "burgertime2.jpg", title : "Burgertime", repo : "https://github.com/mbrennecke/burgertime/"
-	},{
-	link : "https://dry-meadow-80045.herokuapp.com/", image : "burgertime.jpg", title : "Burgertime Sequelized", repo : "https://github.com/mbrennecke/sequelizedburger/"
-	},{
-	link : "https://mbrennecke.github.io/Word-Guess-Game/", image : "words.jpg", title : "Word Guess Game", repo : "https://github.com/mbrennecke/Word-Guess-Game/"
-	},{
 	link : "https://drive.google.com/file/d/1MmfniKQxQepnpFAppvSxRn1ONHO-XfGw/view?usp=sharing", image : "wordguess2.jpg", title : "Word Guess node app", repo : "https://github.com/mbrennecke/constructor-word-guess/"
 	},{
-	link : "https://mbrennecke.github.io/train-scheduler/", image : "train.jpg", title : "Train Schedule", repo : "https://github.com/mbrennecke/train-scheduler/"
-	},{
-	link : "https://drive.google.com/file/d/1nTF2r6afrNXTRzR_GVBle10L7ERjeBJk/view?usp=sharing", image : "bamazon.jpg", title : "Train Schedule", repo : "https://github.com/mbrennecke/bamazon/"
+	link : "https://drive.google.com/file/d/1nTF2r6afrNXTRzR_GVBle10L7ERjeBJk/view?usp=sharing", image : "bamazon.jpg", title : "Bamazon", repo : "https://github.com/mbrennecke/bamazon/"
 	}];
 
 $(document).ready(function() {
